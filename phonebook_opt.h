33791f871dafeb306460174a69b7c4c9d85d87f6
Shawn Lin <bob89069@gmail.com>
2016-03-05 03:52:21 +0800

    update

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -1,9 +1,9 @@
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
-#define OPT 1
 #define MAX_LAST_NAME_SIZE 16
-
+// -- optimized LAST_NAME_ENTRY  struct size
 typedef struct __PHONE_BOOK_ENTRY_NO_LASTNAME {
+    char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
     char phone[10];
@@ -13,10 +13,10 @@ typedef struct __PHONE_BOOK_ENTRY_NO_LASTNAME {
     char city[16];
     char state[2];
     char zip[5];
-};
+} entry_detail;
 
 //optimize struct
-typedef struct __LAST_NAME_ENTRY{
+typedef struct __LAST_NAME_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY_NO_LASTNAME *detail;
     struct __LAST_NAME_ENTRY *pNext;
